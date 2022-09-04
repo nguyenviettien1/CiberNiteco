2aa5e27ba5f15436c843391cf9c17790bc722267
tiennv <viettien98htttbn@gmail.com>
2022-09-04 18:21:22 +0700

    adminWeb

---
+++ CiberNiteco.AdminWeb/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using CiberNiteco.Entities.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiberNiteco.AdminWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
